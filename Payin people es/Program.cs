e018f22a044215bcf2b351176aa76962b3ff9384
Nikicoraz <49104514+Nikicoraz@users.noreply.github.com>
2022-01-22 13:09:56 +0100

    Cambiameti molto belli
    - Tolta inutile variabile extra_paga
    - Tolto obbrobrioso do {...}while vuoto all'inizio del file
    - Input sulla stessa riga piu' bello
    
    TLDR: Accetta or else 🔫

--- Payin people es/Program.cs
+++ Payin people es/Program.cs
@@ -10,19 +10,17 @@ namespace Payin_people_es
     {
         static void Main(string[] args)
         {
-            bool extra_paga = false;
             int n;
-            Console.WriteLine("Insert the number of consultants");
-            do
-            {
+            Console.Write("Insert the number of consultants: ");
 
-            } while (!int.TryParse(Console.ReadLine(), out n));
+            while (!int.TryParse(Console.ReadLine(), out n)) ;
+
             int[] settimane = new int[n], paga = new int[n];
             string[] nomi_consulenti = new string[n];
             //The name of the consultant is inserted
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine($"Insert the number of consultant n°{i}");
+                Console.Write($"Insert the number of consultant n°{i}: ");
                 nomi_consulenti[i] = Console.ReadLine();
             }
             //The pay for the consultant is inserted
@@ -30,7 +28,7 @@ namespace Payin_people_es
             {
                 do
                 {
-                    Console.WriteLine($"Insert he pay of consultant {nomi_consulenti[i]}");
+                    Console.Write($"Insert he pay of consultant {nomi_consulenti[i]}: ");
                 } while (!int.TryParse(Console.ReadLine(), out paga[i]));
             }
             //Get in the input how many weeks they worked
@@ -38,19 +36,14 @@ namespace Payin_people_es
             {
                 do
                 {
-                    Console.WriteLine($"Insert how many weeks {nomi_consulenti[i]} worked");
+                    Console.Write($"Insert how many weeks {nomi_consulenti[i]} worked: ");
                 } while (!int.TryParse(Console.ReadLine(), out settimane[i]));
             }
             //Checks if they had 4 weeks of work and gives extra cash if so
             for (int i = 0; i < n; i++)
             {
-                if (settimane[i] < 4)
+                if (settimane[i] >= 4)
                 {
-                    extra_paga = false;
-                }
-                else
-                {
-                    extra_paga = true;
                     paga[i] += paga[i] / 2;
                 }
                 Console.WriteLine($"The pay of {nomi_consulenti[i]} is {paga[i]}");
